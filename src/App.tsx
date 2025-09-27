0e25c6acd5cb6e73f12e538c2f28bdea414269b6
torihazi <lovebarusa19981225@icloud.com>
2025-09-27 09:33:27 +0900

    固定値のカウントダウン

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // カウントダウンタイマーを作る上で必要になることはなんだろうか？
+  // 1. タイマーの開始と停止
+  // 2. タイマーのリセット
+  // 3. タイマーの表示
+  // 4. タイマーの進行
+  // 5. タイマーの完了
+  // 6. タイマーの表示
+
+  // タイマーは1秒ごとに1sずつ減らしていくもの。
+  // setIntervalを使って1秒ごとに1sずつ減らしていく。
+  // タイマーの機能はUI描画に関係ないことだから useEffectを使う
+  // 秒数を記憶する、つまり状態を記憶する必要があるからuseStateを使う
+  // stateの初期値は何がいいんだろうか。
+  const [time, setTime] = useState(10);
+
+  // 正しく動いているようには見えるけど、毎回setIntervalを作り直している。
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setTime(time - 1);
+  //   }, 1000);
+  //   console.log(id);
+  //   return () => clearInterval(id);
+  // }, [time]);
+
+  // ただ依存配列のtimeを消しても、useEffectの実行時点の古いtimeの値しか参照しないのでtimerは機能しない
+  //
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setTime(time - 1);
+  //   }, 1000);
+  //   console.log(id);
+  //   return () => clearInterval(id);
+  // }, []);
+
+  // この状態だと 0になったら止まるが、裏では0で更新し続けるという挙動になる。
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setTime((prev) => (prev > 0 ? prev - 1 : 0));
+  //   }, 1000);
+  //   return () => clearInterval(id);
+  // }, []);
+
+  // useEffect(() => {
+  //   const id = setInterval(() => {
+  //     setTime((prev) => {
+  //       if (prev <= 1) {
+  //         clearInterval(id); // ここで止める！
+  //         return 0;
+  //       }
+  //       return prev - 1;
+  //     });
+  //   }, 1000);
+
+  //   return () => clearInterval(id);
+  // }, []);
+
+  // 本当にstateは更新されていないか、intevalはクリアされているのかを確認する方法はあるのか。
+  //stateが更新される => レンダリングされる => console.logを差し込んでも良い
+  // クリアされているかは クリーンアップが呼ばれているかしか確認できる術はなさそう。
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime((prev) => {
+        if (prev <= 1) {
+          console.log("止まるよ");
+          console.log(id);
+          clearInterval(id);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+    return () => {
+      console.log("クリアされるよ");
+      clearInterval(id);
+    };
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>{time}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
